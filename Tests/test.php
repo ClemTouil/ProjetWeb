e146dedcbb6bd22530757a64211b1f8a2b882632
Anass DOULACHE <anass.doulache@etud.univ-jfc.fr>
2019-03-29 22:41:12 +0100

    Test : Traitement de donnees

---
+++ Tests/test.php
@@ -0,0 +1,21 @@
+<?php
+
+    define('HOSTNAME','localhost');
+    define('DB_USERNAME','root');
+    define('DB_PASSWORD','');
+    define('DB_NAME','projet_web');
+
+    $mysqli = new mysqli(HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    if ($mysqli->connect_errno) { // Si il y'a une erreur de connexion alors :
+        printf("Échec de la connexion : %s\n", $mysqli->connect_error);
+        exit();
+    }else { // Si la connexion se passe bien alors :
+
+        $result = mysqli_query($mysqli,"SELECT nom_q FROM quartiers ");
+
+        while ( $row = mysqli_fetch_array($result)) {
+            echo '<option value= "'.$row['nom_q'].'" >'.$row['nom_q'].'</option>';
+        }
+    }
+?>
